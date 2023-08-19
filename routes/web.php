9eaba1ba30b153589971078f424d9a7230c88234
Anisha <acharyanisha025@gmail.com>
2023-08-19 22:18:08 +0545

    product added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\EditProfileController;
+use App\Http\Controllers\FrontendController;
+use App\Http\Controllers\ProductController;
 use App\Models\Editprofile;
 use Illuminate\Support\Facades\Route;
 
@@ -18,23 +20,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/product', [FrontendController::class, 'product']);
+Route::get('/addproduct', [FrontendController::class, 'addproduct']);
 
+
+Route::post('/store-product', [ProductController::class, 'store']);
+Route::get('/delete-product/{id}', [ProductController::class, 'destroy']);
+Route::post('/update-product/{id}', [ProductController::class, 'update']);
+Route::get('/edit-product{id}', [ProductController::class, 'edit']);
 
 Auth::routes();
 
-Route::group(['middleware'=>["auth","admin"]],function()
-{
+Route::group(['middleware' => ["auth", "admin"]], function () {
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-   
-
 });
 
-Route::group(['middleware'=>["auth"]],function()
-{
-   // edit profile
-Route::get('/create',[EditProfileController::class,'create']);
-Route::post('/update-profile',[EditProfileController::class,'editProfile'])->name('update-profile');;
-Route::get('/view-profile',[EditProfileController::class,'viewProfile']);
-
+Route::group(['middleware' => ["auth"]], function () {
+    // edit profile
+    Route::get('/create', [EditProfileController::class, 'create']);
+    Route::post('/update-profile', [EditProfileController::class, 'editProfile'])->name('update-profile');;
+    Route::get('/view-profile', [EditProfileController::class, 'viewProfile']);
 });
