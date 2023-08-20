6399fefd64da97f22029a105b2b4f697e6a9b661
bipana <bipanashrestha435@gmail.com>
2023-08-20 11:37:43 +0545

    fetch category products to user side

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -13,19 +13,22 @@
       <h1 class="product-heading">Products</h1>
 
       <div class="product-list">
+        @foreach($products as $product)
         <div class="box">
           <div class="card">
             <img
-              src="/Image/cloth.jpg"
+              src="{{asset('$product->image')}}"
               alt="Denim Jeans"
               style="width: 260px; height: 300px"
             />
-            <h2>Chiffon Floral Printed</h2>
-            <p class="price">Rs. 1,399</p>
-            <p class="brand">Brand</p>
+            <h2>{{$product->productName}}</h2>
+            <p class="price">{{$product->price}}</p>
+            <p class="brand">{{$product->brand}}</p>
+            <p class="stock">{{$product->category['name']}}</p>
             <p><button>Add to Cart</button></p>
           </div>
         </div>
+        @endforeach
       </div>
     </div>
 @endsection
