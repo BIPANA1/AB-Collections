f704cc808730ca59d9aff0e1b3010ff2dfbb1e47
bipana <bipanashrestha435@gmail.com>
2023-08-20 15:54:08 +0545

    changes in home page also fetch data from database

---
+++ resources/views/user/product.blade.php
@@ -0,0 +1,85 @@
+@extends('user.layout.main')
+@section('content')
+<style>
+    .card {
+        margin-top: 45%;
+        width: 300px;
+    }
+
+    img {
+        margin-left: 8px;
+    }
+    .filter-dropdown {
+            position: absolute;
+            top: 15%; /* Adjust this value as needed */
+            margin-left: 80%;
+        }
+
+        .dropdown-btn {
+            background-color: white;
+            border: 1px solid #ccc;
+            color: #333;
+            padding: 5px 10px;
+            cursor: pointer;
+            display: inline-block;
+        }
+
+        .dropdown-menu {
+            display: none;
+            position: absolute;
+            background-color: white;
+            border: 1px solid #ccc;
+            width: 150px;
+            z-index: 1;
+        }
+
+        .dropdown-btn:hover .dropdown-menu {
+            display: block;
+        }
+
+        .dropdown-item {
+            padding: 8px 10px;
+            text-decoration: none;
+            color: #333;
+            display: block;
+        }
+
+        .dropdown-item:hover {
+            background-color: #f2f2f2;
+        }
+    
+   
+</style>
+
+<!-- Beautiful Dropdown Menu -->
+<div class="filter-dropdown">
+        <div class="dropdown-btn">
+            <span class="fa-solid fa-filter"></span> Filter
+            <div class="dropdown-menu">
+                <a class="dropdown-item" href="/show-category">Category</a>
+                <a class="dropdown-item" href="/show-highestprice">Highest Price</a>
+            </div>
+        </div>
+    </div>
+
+
+
+<div class="product-list">
+    @foreach($products as $product)
+    <div class="box">
+        <div class="card">
+            <img src="{{asset($product->image)}}" alt="Denim Jeans" style="width: 260px; height: 300px" />
+            <h2>{{$product->productName}}</h2>
+            <p class="price">{{$product->price}}</p>
+            <p class="brand">{{$product->brand}}</p>
+            <p class="stock">{{$product->category['name']}}</p>
+            <p><button>Add to Cart</button></p>
+        </div>
+    </div>
+    @endforeach
+</div>
+
+
+
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,34 +45,9 @@
               </li>
               <li><a style="font-size: 23px" href="#">View Cart</a></li>
 
-              @if(auth()->user())
-              <div style="margin-left: 30px">
-                <img class="user-img" onclick="toggleDropdown();" src="/Image/image.png" alt="" />
-                <div class="click-img" id="dropdown">
-                  <!-- <a href="/edit-profile">Edit Profile</a>
-                            <a href="#">View Profile</a> -->
-                  @if(auth()->user())
-                  <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                  </a>
-
-                  <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                  </form>
-                  @else
-
-                  <a class="nav-item">
-                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                  </a>
-                  <a class="nav-item">
-                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                  </a>
-                  @endif
-                  @else
                   <li><a style="font-size: 23px" href="/login">Login</a></li>
                   <li><a style="font-size: 23px" href="/register">Register</a></li>
-                  @endif
+              
             </ul>
           </div>
         </div>
@@ -91,31 +66,31 @@
       </div> -->
   </div>
   </div>
-
   <div class="products">
     <h1 class="product-heading">Products</h1>
 
-
     <div class="product-list">
+      @foreach($products as $product)
       <div class="box">
         <div class="card">
-          <img src="/Image/cloth.jpg" alt="Denim Jeans" style="width: 260px; height: 300px" />
-          <h2>Chiffon Floral Printed</h2>
-          <p class="price">Rs. 1,399</p>
-          <p class="brand">Brand</p>
+          <img src="{{asset($product->image)}}" alt="Denim Jeans" style="width: 260px; height: 300px" />
+          <h2></h2>
+          <p class="price">{{$product->price}}</p>
+          <p class="brand">{{$product->brand}}</p>
+          <p class="stock">{{$product->category['name']}}</p>
           <p><button>Add to Cart</button></p>
         </div>
       </div>
-    </div>
-  </div>
+      @endforeach
 
-  <script>
-    function toggleDropdown() {
-      var dropdown = document.getElementById("dropdown");
-      dropdown.style.display =
-        dropdown.style.display === "none" ? "block" : "none";
-    }
-  </script>
+
+      <script>
+        function toggleDropdown() {
+          var dropdown = document.getElementById("dropdown");
+          dropdown.style.display =
+            dropdown.style.display === "none" ? "block" : "none";
+        }
+      </script>
 </body>
 
 </html>
