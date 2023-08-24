86e18aead1dc6cf2479815f98eb50c8b0c4930d7
bipana <bipanashrestha435@gmail.com>
2023-08-24 16:43:14 +0545

    changes in searchController

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,30 +7,8 @@ use Illuminate\Http\Request;
 
 class SearchController extends Controller
 {
-    public function search(Request $request) {
-        $query = Product::query();
-    
-        // Filter by category
-        if ($request->has('category') && !empty($request->category)) {
-            $query->where('category', $request->category);
-        }
-    
-        // Filter by price range
-        if ($request->has('min_price') && !empty($request->min_price)) {
-            $query->where('price', '>=', $request->min_price);
-        }
-        if ($request->has('max_price') && !empty($request->max_price)) {
-            $query->where('price', '<=', $request->max_price);
-        }
-    
-        // Filter by brand
-        if ($request->has('brand') && !empty($request->brand)) {
-            $query->where('brand', $request->brand);
-        }
-    
-        $products = $query->get();
-    
-        return view('user.product', compact('products'));
+    public function search(Request $request){
+        $products = product::where('category_id', 'like', '%'.$request->search.'%')->orwhere('brand', 'like', '%'.$request->search.'%')->orwhere('price', 'like', '%'.$request->search.'%')->get();
+        return view('user.product',compact('products'));
     }
-    
 }
